aced22cf909f61d278add13fc7e66af8d16fd453
Ranjan Shrestha <ranjan@sharingapples.com>
2017-08-24 21:28:02 +0545

    Catch error on rpc server avoiding any crashes

--- packages/rpc/src/Server.js
+++ packages/rpc/src/Server.js
@@ -3,13 +3,21 @@ import { ERROR, CALLBACK, REQUEST, RESPONSE, CALLBACK_VALIDATOR } from './consta
 export default function configureServer(WebSocketServer) {
   return {
     bind(apis, implementations) {
-      this.apis = Object.keys(apis).reduce((res, apiName) => ({
-        ...res,
-        [apiName]: {
-          fn: (...args) => implementations[apiName](...args),
-          signature: apis[apiName](),
-        },
-      }), {});
+      this.apis = Object.keys(apis).reduce((res, apiName) => {
+        // Make sure there is an implementation
+        const implementation = implementations[apiName];
+        if (typeof implementation !== 'function') {
+          throw new Error(`Implementation not found for api "${apiName}"`);
+        }
+
+        return {
+          ...res,
+          [apiName]: {
+            fn: (...args) => implementation(...args),
+            signature: apis[apiName](),
+          },
+        };
+      }, {});
     },
 
     start(port) {
@@ -33,24 +41,37 @@ export default function configureServer(WebSocketServer) {
           }));
         };
 
-        ws.on('message', (data) => {
-          const msg = JSON.parse(data);
-          if (msg.type === REQUEST) {
-            const api = this.apis[msg.name];
-            if (!api) {
-              sendError(msg.id, msg.name, 'API not found on server');
-              return;
-            }
+        const parseResponse = (data) => {
+          try {
+            const msg = JSON.parse(data);
+            return msg;
+          } catch (e) {
+            sendError('', '', 'Invalid request data. Expected JSON');
+            return false;
+          }
+        };
 
-            const args = this.processArgs(msg.args, api, ws);
-            // Execute the method
-            Promise.resolve(api.fn.apply(null, args)).then((res) => {
-              sendResponse(msg.id, msg.name, res);
-            }).catch((err) => {
-              sendError(msg.id, msg.name, err.message);
-            });
-          } else {
-            sendError(msg.id, msg.name, `Server can't handle message of type ${msg.type}`);
+        ws.on('message', (data) => {
+          const msg = parseResponse(data);
+          if (msg !== false) {
+            if (msg.type === REQUEST) {
+              const api = this.apis[msg.name];
+              if (!api) {
+                sendError(msg.id, msg.name, 'API not found on server');
+                return;
+              }
+
+              const args = this.processArgs(msg.args, api, ws);
+
+              // Execute the method
+              Promise.resolve(api.fn.apply(null, args)).then((res) => {
+                sendResponse(msg.id, msg.name, res);
+              }).catch((err) => {
+                sendError(msg.id, msg.name, err.message);
+              });
+            } else {
+              sendError(msg.id, msg.name, `Server can't handle message of type ${msg.type}`);
+            }
           }
         });
       });
